bb51c0e492d3778ff301c1636ee7b6fa631eab49
Rplus <rplus.tw@gmail.com>
2018-11-17 19:04:01 +0800

    deploy

---
+++ precache-manifest.50485ac6103b07dadb151df0830b243a.js
@@ -0,0 +1,294 @@
+self.__precacheManifest = [
+  {
+    "revision": "165de80121e8b2c8764c9842d2ade556",
+    "url": "/Pokemon-CP-list/img/pm/492-land.png"
+  },
+  {
+    "revision": "f31dd0674f3aadd96bfce608f0f6906b",
+    "url": "/Pokemon-CP-list/favicon.png"
+  },
+  {
+    "revision": "fa0b37e2c945abdc2d78",
+    "url": "/Pokemon-CP-list/js/chunk-vendors.e580ad60.js"
+  },
+  {
+    "revision": "6ddd7e57459d058a08198dbca4da4fbc",
+    "url": "/Pokemon-CP-list/img/51-alola.6ddd7e57.png"
+  },
+  {
+    "revision": "595539cc92f846a38ad34776e46097e6",
+    "url": "/Pokemon-CP-list/img/75-alola.595539cc.png"
+  },
+  {
+    "revision": "3ef15b51ed5d9c05e7c29a2e3c12daca",
+    "url": "/Pokemon-CP-list/img/sprite1-4.3ef15b51.png"
+  },
+  {
+    "revision": "1ad38989e13830b3b314870d62784224",
+    "url": "/Pokemon-CP-list/img/89-alola.1ad38989.png"
+  },
+  {
+    "revision": "940811cf5872406d7bc27c0f449f194b",
+    "url": "/Pokemon-CP-list/img/487-altered.940811cf.png"
+  },
+  {
+    "revision": "481d6a2d3a3d434a46c2b3b08d8eaf4a",
+    "url": "/Pokemon-CP-list/img/76-alola.481d6a2d.png"
+  },
+  {
+    "revision": "f4ec57cd4c92f31d0234a3c630a2c1e7",
+    "url": "/Pokemon-CP-list/img/53-alola.f4ec57cd.png"
+  },
+  {
+    "revision": "df890166443c2c42d90f5f6026050a21",
+    "url": "/Pokemon-CP-list/img/38-alola.df890166.png"
+  },
+  {
+    "revision": "83a2b35e5fde8f0d2de76f9f30485e7d",
+    "url": "/Pokemon-CP-list/index.html"
+  },
+  {
+    "revision": "401ac3f5d033b40866ee87c4dd83af75",
+    "url": "/Pokemon-CP-list/img/pm/103-alola.png"
+  },
+  {
+    "revision": "538ea192d069d8fdced1c9454c33bd70",
+    "url": "/Pokemon-CP-list/img/pm/105-alola.png"
+  },
+  {
+    "revision": "d6daa5ad21950ffda047ff35b68b106b",
+    "url": "/Pokemon-CP-list/img/pm/19-alola.png"
+  },
+  {
+    "revision": "1052acf43fa9565c93a0b248db965687",
+    "url": "/Pokemon-CP-list/img/pm/20-alola.png"
+  },
+  {
+    "revision": "d1fe26a1744794dd4383d896ea69c45b",
+    "url": "/Pokemon-CP-list/img/pm/26-alola.png"
+  },
+  {
+    "revision": "67267c47ce418c139ecdc5ef65539cf6",
+    "url": "/Pokemon-CP-list/img/pm/27-alola.png"
+  },
+  {
+    "revision": "65468b3f5ee6b1e175ef4b51ebe4a596",
+    "url": "/Pokemon-CP-list/img/pm/28-alola.png"
+  },
+  {
+    "revision": "7cf683b632ed8a2fe0e98ef924771b6b",
+    "url": "/Pokemon-CP-list/img/pm/37-alola.png"
+  },
+  {
+    "revision": "df890166443c2c42d90f5f6026050a21",
+    "url": "/Pokemon-CP-list/img/pm/38-alola.png"
+  },
+  {
+    "revision": "3d24e7d5d593850f68088ef43b236604",
+    "url": "/Pokemon-CP-list/img/pm/386-attack.png"
+  },
+  {
+    "revision": "e790ae0037a050157ff2fb363944a5be",
+    "url": "/Pokemon-CP-list/img/pm/386-defense.png"
+  },
+  {
+    "revision": "be6fd62e25973e3c655120fc2fc8232b",
+    "url": "/Pokemon-CP-list/img/pm/386-normal.png"
+  },
+  {
+    "revision": "c619fb59fadfaebcafc935022dd18983",
+    "url": "/Pokemon-CP-list/img/pm/386-speed.png"
+  },
+  {
+    "revision": "0c4edd1be354394820f70fffa98b4f3f",
+    "url": "/Pokemon-CP-list/img/pm/413-plant.png"
+  },
+  {
+    "revision": "f61332a9eba5fb0f116215e740c9312e",
+    "url": "/Pokemon-CP-list/img/pm/413-sandy.png"
+  },
+  {
+    "revision": "8a7a181cb81a274f17822b0ee0621d6a",
+    "url": "/Pokemon-CP-list/img/pm/413-trash.png"
+  },
+  {
+    "revision": "ec6ff80aee2cca4b812aafa81bdc6bfb",
+    "url": "/Pokemon-CP-list/img/pm/479-fan.png"
+  },
+  {
+    "revision": "94065d38fb058c01a70cae7c33a0960f",
+    "url": "/Pokemon-CP-list/img/pm/479-frost.png"
+  },
+  {
+    "revision": "123fed157f06142a7d609db3a7e5e601",
+    "url": "/Pokemon-CP-list/img/pm/479-heat.png"
+  },
+  {
+    "revision": "2b4f92d4f7abdce6438b0c50f8c32ca4",
+    "url": "/Pokemon-CP-list/img/pm/479-mow.png"
+  },
+  {
+    "revision": "563473713daecfc989c180172b2d94cd",
+    "url": "/Pokemon-CP-list/img/pm/479-normal.png"
+  },
+  {
+    "revision": "4665576b531383a8df3876dde54be2b9",
+    "url": "/Pokemon-CP-list/img/pm/479-wash.png"
+  },
+  {
+    "revision": "940811cf5872406d7bc27c0f449f194b",
+    "url": "/Pokemon-CP-list/img/pm/487-altered.png"
+  },
+  {
+    "revision": "39dd96a119618c62181c1a28bd5abb3a",
+    "url": "/Pokemon-CP-list/img/pm/487-origin.png"
+  },
+  {
+    "revision": "cc1eec124a983efc0766",
+    "url": "/Pokemon-CP-list/js/app.c81203a4.js"
+  },
+  {
+    "revision": "166c432e5ba90f866ed3dc2b04372f69",
+    "url": "/Pokemon-CP-list/img/pm/492-sky.png"
+  },
+  {
+    "revision": "be17560143dce15d25124fd62853ee04",
+    "url": "/Pokemon-CP-list/img/pm/50-alola.png"
+  },
+  {
+    "revision": "6ddd7e57459d058a08198dbca4da4fbc",
+    "url": "/Pokemon-CP-list/img/pm/51-alola.png"
+  },
+  {
+    "revision": "ef59f899dad53b4e57ab293e75aaba3d",
+    "url": "/Pokemon-CP-list/img/pm/52-alola.png"
+  },
+  {
+    "revision": "f4ec57cd4c92f31d0234a3c630a2c1e7",
+    "url": "/Pokemon-CP-list/img/pm/53-alola.png"
+  },
+  {
+    "revision": "3db64395d523e30a476d7620a56fbc37",
+    "url": "/Pokemon-CP-list/img/pm/74-alola.png"
+  },
+  {
+    "revision": "595539cc92f846a38ad34776e46097e6",
+    "url": "/Pokemon-CP-list/img/pm/75-alola.png"
+  },
+  {
+    "revision": "481d6a2d3a3d434a46c2b3b08d8eaf4a",
+    "url": "/Pokemon-CP-list/img/pm/76-alola.png"
+  },
+  {
+    "revision": "668ca635d472743ad640574e713ca7d3",
+    "url": "/Pokemon-CP-list/img/pm/808.png"
+  },
+  {
+    "revision": "b563f9b852a26b9f74a572d6154e35bf",
+    "url": "/Pokemon-CP-list/img/pm/809.png"
+  },
+  {
+    "revision": "1e4d65fbfec4d29475ae00bf1a3b012a",
+    "url": "/Pokemon-CP-list/img/pm/88-alola.png"
+  },
+  {
+    "revision": "1ad38989e13830b3b314870d62784224",
+    "url": "/Pokemon-CP-list/img/pm/89-alola.png"
+  },
+  {
+    "revision": "3ef15b51ed5d9c05e7c29a2e3c12daca",
+    "url": "/Pokemon-CP-list/img/sprite1-4.png"
+  },
+  {
+    "revision": "4ea58423010a035c47fbf6fc820764aa",
+    "url": "/Pokemon-CP-list/img/type/type_alola.png"
+  },
+  {
+    "revision": "ee9bbff174a64810fdd3bbd0e2866827",
+    "url": "/Pokemon-CP-list/img/type/type_bug.png"
+  },
+  {
+    "revision": "d176e2d931b2fec577c098a45b332655",
+    "url": "/Pokemon-CP-list/img/type/type_dark.png"
+  },
+  {
+    "revision": "4ed020b3f369230850a11cbd3574b020",
+    "url": "/Pokemon-CP-list/img/type/type_dragon.png"
+  },
+  {
+    "revision": "2007d1c40e25a432322f362237349efa",
+    "url": "/Pokemon-CP-list/img/type/type_electric.png"
+  },
+  {
+    "revision": "23a9f1b08f7389f9ceeba047a36413a2",
+    "url": "/Pokemon-CP-list/img/type/type_fairy.png"
+  },
+  {
+    "revision": "d82ccf983c6417d856ebe985a8bd96fa",
+    "url": "/Pokemon-CP-list/img/type/type_fighting.png"
+  },
+  {
+    "revision": "4183dcca74ea3ca43d990489920d89ee",
+    "url": "/Pokemon-CP-list/img/type/type_fire.png"
+  },
+  {
+    "revision": "a2df0e0b7bbabc1cc259213eb16cb97e",
+    "url": "/Pokemon-CP-list/img/type/type_flying.png"
+  },
+  {
+    "revision": "de608c53e7b54a9b859bacee5b726841",
+    "url": "/Pokemon-CP-list/img/type/type_ghost.png"
+  },
+  {
+    "revision": "1512a5c9622aac8444f4e39c377d26e7",
+    "url": "/Pokemon-CP-list/img/type/type_grass.png"
+  },
+  {
+    "revision": "3ce071baf4031762251fcca9dbb61729",
+    "url": "/Pokemon-CP-list/img/type/type_ground.png"
+  },
+  {
+    "revision": "ea3a6bb72511f45e41d66c006ff5871f",
+    "url": "/Pokemon-CP-list/img/type/type_ice.png"
+  },
+  {
+    "revision": "4ea58423010a035c47fbf6fc820764aa",
+    "url": "/Pokemon-CP-list/img/type/type_normal.png"
+  },
+  {
+    "revision": "03639a13d435bc556af449db2b588581",
+    "url": "/Pokemon-CP-list/img/type/type_poison.png"
+  },
+  {
+    "revision": "b370f250643aa13b00d4df8f5ad0d71f",
+    "url": "/Pokemon-CP-list/img/type/type_psychic.png"
+  },
+  {
+    "revision": "71f2bc934f58aacb313d94593f1d25b5",
+    "url": "/Pokemon-CP-list/img/type/type_rarity.png"
+  },
+  {
+    "revision": "b43640ed2026a5a3aaf60f4312f2c15a",
+    "url": "/Pokemon-CP-list/img/type/type_rock.png"
+  },
+  {
+    "revision": "9acfd4aa245c1ee0a51819d98d0f5391",
+    "url": "/Pokemon-CP-list/img/type/type_steel.png"
+  },
+  {
+    "revision": "6a4f8dd8cc02d0421670a32405b47f0a",
+    "url": "/Pokemon-CP-list/img/type/type_water.png"
+  },
+  {
+    "revision": "5a06b9339d2af359e4edf9b7370dfc3b",
+    "url": "/Pokemon-CP-list/img/pm/makefile"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Pokemon-CP-list/robots.txt"
+  },
+  {
+    "revision": "cc1eec124a983efc0766",
+    "url": "/Pokemon-CP-list/css/app.f0808dcb.css"
+  }
+];
